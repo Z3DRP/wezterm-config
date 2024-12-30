05ea69e7867a2d04fa7559b1bfc0ac70cc7f98cd
Zach Palmer <apex1421@outlook.com>
2024-12-30 09:24:19 -0600

    new config updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,30 +11,24 @@ config.window_frame = {
 	font_size = 12.0,
 }
 
-config.color_schemes = {
-	["tfox"] = {
-		foreground = "#d0d0d0",
-		background = "#1c1c1c",
-		cursor_bg = "#d0d0d0",
-		cursor_fg = "#1c1c1c",
-		selection_bg = "#444444",
-		selection_fg = "#ffffff",
-		ansi = { "#1c1c1c", "#d70000", "#5f8700", "#af8700", "#0087ff", "#af005f", "#00afaf", "#d0d0d0" },
-		brights = { "#505050", "#ff0000", "#87af00", "#ffaf00", "#0087ff", "#af005f", "#00afaf", "#e4e4e4" },
-	},
-}
 --config.color_scheme = "Breath Darker (Gogh)"
---config.color_scheme = "tfox"
+config.color_scheme = "terafox"
 --config.color_scheme = "zenwritten_dark"
 --config.color_scheme = "Afterglow (Gogh)"
-config.color_scheme = "Atelier Savanna (base16)"
+--config.color_schemes = "batman"
+--config.color_scheme = "Atelier Savanna (base16)"
+--config.color_scheme_dirs = { "~/.config/wezterm/colors/darkforest.toml" }
+--config.color_scheme = "Rosé Pine (Gogh)"
+--config.color_scheme = "Rosé Pine Moon (Gogh)"
+--config.color_scheme = "Terminix Dark (Gogh)"
 config.keys = {
 	{ key = "f", mods = "CMD", action = "ToggleFullScreen" },
 	{ key = "t", mods = "CMD", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-	{ key = "d", mods = "CMD", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+	{ key = "c", mods = "CMD", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
 	{ key = "b", mods = "CMD", action = wezterm.action({ ActivateTabRelative = -1 }) },
 	{ key = "n", mods = "CMD", action = wezterm.action({ ActivateTabRelative = 1 }) },
 	{ key = "e", mods = "CMD", action = wezterm.action.QuitApplication },
+	{ key = "x", mods = "CMD", action = wezterm.action.SendKey({ key = "Escape" }) },
 }
 
 return config
